c272af43d8a4ed37eb87f7873b8d76fc1774e46a
proDream <sushkoos@gmail.com>
2022-09-28 16:21:04 +0400

    Added program in task directory

---
+++ task/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] SearchElemets(string[] array)
+{
+    string[] result = new string[0];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            if (result.Length < i)
+            {
+                Array.Resize(ref result, result.Length + 1);
+            }
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+
+Console.Write("Введите элементы массива разделив их запятой и  пробелом: ");
+string text = Console.ReadLine() ?? String.Empty;
+string[] textArray = text.Split(", ");
+string[] resultArray = SearchElemets(textArray);
+Console.WriteLine($"[{String.Join(", ", resultArray)}]");
